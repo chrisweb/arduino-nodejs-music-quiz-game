083bc7232d43e07d68c971475caba18406156221
Xavier-Cliquennois <xavier.cliquennois@live.fr>
2017-01-29 19:27:02 +0100

    insert propo in project

--- client/library/controllers/gameMasterScreen.ts
+++ client/library/controllers/gameMasterScreen.ts
@@ -7,6 +7,8 @@ import * as io from 'socket.io-client';
 
 export class GameMasterScreenController {
 
+    protected _socket: any;
+
     public constructor() {
 
 
@@ -19,13 +21,22 @@ export class GameMasterScreenController {
 
         $body.empty();
 
-        $body.append('Hello Game Master');
+        let $container = $('<div id="container">');
+        $body.append($container);
+
+        // init heigth of container
+        $container.height($(window).height());
+        // dynamic resize
+        $(window).resize(function() {
+            $('#container').height($(window).height());
+        });
+
 
 
         // socket.io
-        let socket = io.connect('http://127.0.0.1:35001');
+        this._socket = io.connect('http://127.0.0.1:35001');
 
-        let message = 'hello world';
+        /*let message = 'hello world';
 
         console.log('sending message: ' + message);
 
@@ -35,8 +46,180 @@ export class GameMasterScreenController {
 
             console.log('response message recieved: ' + responseMessage);
 
+        });*/
+
+        // on server send event 'newSongStart'
+        this._socket.on('newSongStart', function onNewSongStart(event: any) {
+            this._displayValidateBtn(false);
+            this._displayPageGame(event.trackTitle, event.artistName);
         });
 
+        // on server send event 'playlistFinished'
+        this._socket.on('playlistFinished', function onPlaylistFinished(event: any) {
+            this._displayValidateBtn(false);
+            this._displayPageStart();
+        });
+
+        // on server send event 'playerPressButton'
+        this._socket.on('playerPressButton', function onPlayerPressButton(event: any) {
+            this._displayValidateBtn(true);
+        });
+
+    }
+
+    protected _displayPageStart() {
+        let $container = $('#container');
+
+        $container.empty();
+
+        let $pageStart = $('<div id="page_start">');
+        let btnStart = $('<button class="js-start-btn">').text('Start new game');
+        $pageStart.append(btnStart);
+
+        $container.append($pageStart);
+
+
+        // init button start game
+        $pageStart.on('click', '.js-start-btn', function onClickStartBtnFunction(event) {
+            event.preventDefault();
+
+            this._displayPageSetGame();
+        });
+    }
+
+    protected _displayPageWait() {
+        let $container = $('#container');
+
+        $container.empty();
+
+        let $page = $('<div id="page_wait">');
+        let $title = $('<h1>').text('PLEASE WAIT');
+        $page.append($title);
+
+        $container.append($page);
+    }
+
+    protected _displayPageSetGame() {
+        let $container = $('#container');
+
+        $container.empty();
+
+        let $pageSetGame = $('<div id="page_set_game">');
+        
+        let $form = $('<form id="set_team_and_playlist">');
+        $form.append($('<h1>').text('Set teams names'));
+
+        for (let i: number = 0; i < 4; ++i) {
+            $form.append($('<label for="teamName' + i + '">').text('team ' + i));
+            $form.append($('<input type="text" id="teamName' + i + '">'));
+            $form.append($('<label for="teamScore' + i + '">').text('team 1'));
+            $form.append($('<input type="text" id="teamScore' + i + '">')).val(0);
+            $form.append($('<br><br>'));
+        }
+
+        let $selectPlaylistInput = $('<select id="playlistId">');
+        $selectPlaylistInput.append($('<option value="rock">').text('rock'));
+        $selectPlaylistInput.append($('<option value="dance">').text('dance'));
+        $form.append($selectPlaylistInput);
+        $form.append($('<input type="submit">'));
+
+        $pageSetGame.append($form);
+
+        $container.append($pageSetGame);
+
+        
+        $form.on('submit', function onSubmitSetTeamAndPlaylist(event) {
+            event.preventDefault();
+
+            // TODO get form info and send it to server
+
+            // on server send event 'newSongStart'
+            this.socket.emit('newSongStart');
+
+            this._displayPageGame();
+            //----------------------------------------
+        }); 
+    }
+
+    protected _displayPageGame(trackTitle: string, artistName: string) {
+        let $container = $('#container');
+
+        $container.empty();
+
+        let $pageGame = $('<div id="page_game">');
+        
+        $pageGame.append('Current track:<br>');
+        $pageGame.append($('<span class="js-current-track-title">'));
+        $pageGame.append($('<span class="js-current-track-artist">'));
+        
+        let $validBtnContainer = $('<div class="js-valide-answer hidden">');
+        $validBtnContainer.append($('<button class="js-good">').text('Correct'));
+        $validBtnContainer.append($('<button class="js-bad">').text('Uncorrect'));
+        $pageGame.append($validBtnContainer);
+        
+        $pageGame.append($('<br><br>'));
+        $pageGame.append($('<button class="js-next-track">').text('Next Track'));
+        
+        $pageGame.append($('<br><br>'));
+        $pageGame.append($('<button class="js-end-game">').text('End the game'));
+
+        $container.append($pageGame);
+
+
+        $pageGame.find('.js-current-track-title').text(trackTitle);
+        $pageGame.find('.js-current-track-artist').text(artistName);
+
+        $pageGame.on('click', '.js-next-track', function onClickBtnNextTrackFunction(event) {
+            event.preventDefault();
+
+            // send to server event 'nextTrack'
+            this.socket.emit('nextTrack');
+
+            this._displayValidateBtn(false);
+            this._displayPageWait();
+        });
+
+        $pageGame.on('click', '.js-end-game', function onClickBtnEndGameFunction(event) {
+            event.preventDefault();
+
+            if (confirm('End the game (go to score screen)?')) {
+            
+                // send to server event 'endGame'
+                this.socket.emit('endGame');
+
+                this._displayValidateBtn(false);
+                this._displayPageStart();
+            }
+        
+        });
+
+    }
+
+    protected _displayValidateBtn(display: boolean) {
+        let $btnContainer = $('#page_game .js-valide-answer');
+
+        if (display === true) {
+            $btnContainer.removeClass('hidden');
+            $btnContainer.on('click', '.js-good', function onClickGoodBtnFunction(event) {
+                event.preventDefault();
+
+                // send to server event 'answerIsValide'
+                this.socket.emit('answerIsValide');
+            });
+
+            $btnContainer.on('click', '.js-bad', function onClickBadBtnFunction(event) {
+                event.preventDefault();
+
+                // send to server event 'answerIsUnvalide'
+                this.socket.emit('answerIsUnvalide');
+            });
+        } else {
+            $btnContainer.addClass('hidden');
+
+            $btnContainer.off('click', '.js-good');
+
+            $btnContainer.off('click', '.js-bad');
+        }
     }
 
 }
--- client/library/controllers/playerScreen.ts
+++ client/library/controllers/playerScreen.ts
@@ -20,6 +20,10 @@ import autoInit from '@material/auto-init/dist/mdc.autoInit';
 
 export class PlayerScreenController {
 
+    protected _timerInterval: number;
+    protected _timerDuration: number = 15;
+    protected _socket: any;
+
     public constructor() {
 
         
@@ -32,7 +36,7 @@ export class PlayerScreenController {
 
         $body.empty();
 
-        $body.append('Hello Player');
+        /*$body.append('Hello Player');
 
         // icons test
         $body.append('<i class="material-icons md-18">face</i>');
@@ -45,13 +49,60 @@ export class PlayerScreenController {
         $body.append('<button class="mdc-button mdc-button--accent">Colored button</button>');
         $body.append('<button class="mdc-button mdc-button--raised">Raised button</button>');
         $body.append('<button class="mdc-button mdc-button--raised" disabled>Raised disabled button</button>');
-
-        let $container = $('<div class="bar">').addClass('foo');
-        let $span = $('<span>');
-        $container.append($span);
+*/
+        let $container = $('<div id="container">');
         $body.append($container);
 
+        // init heigth of container
+        $container.height($(window).height());
+        // dynamic resize
+        $(window).resize(function() {
+            $('#container').height($(window).height());
+        });
+
         this._intializeMaterialDesignComponents();
+
+        // socket.io
+        this._socket = io.connect('http://127.0.0.1:35001');
+
+        // on server send event 'playerPressButton' the lead
+        this._socket.on('playerPressButton', function onPlayerPressButton(event: any) {
+            let $pageGame = $('#page_game');
+            let allPlayers = $pageGame.find('.js-player-container');
+
+            // display lock effect
+            allPlayers.removeClass('active').addClass('lock');
+
+            // display player press effet
+            let $activePlayer = $pageGame.find('[data-player-id="' + event.data.playerId + '"]');
+            $activePlayer.removeClass('lock').addClass('active');
+
+            // start timer
+            let $timer = $pageGame.find('.js-timer');            
+            $timer.removeClass('hidden');
+            $timer.find('.js-timer-count').text(this._timerDuration);
+
+            this._timerInterval = setInterval(function on_timerInterval(){
+                let currentValue: number = parseInt($timer.find('.js-timer-count').text());
+                $timer.find('.js-timer-count').text(currentValue - 1);
+
+                // hide timer and call server with event 'playerTimerFinish'
+                if (currentValue - 1 < 0) {
+                    clearInterval(this._timerInterval);
+
+                    // send event 'playerTimerFinish' to server
+                    socket.emit('playerTimerFinish');
+
+                    $timer.addClass('hidden');
+
+                    // reset other player and lock active current player because he doesn't answer
+                    allPlayers.removeClass('lock');
+                    $activePlayer.removeClass('active').addClass('lock');
+                }
+
+            }, 1000);
+
+        });
 
     }
 
@@ -70,4 +121,96 @@ export class PlayerScreenController {
 
     }
 
+    protected _displayPageStart() {
+        let $container = $('#container');
+
+        $container.empty();
+
+        let $page = $('<div id="page_start">');
+        $page.append($('<h1>QUIZZ GAME !!!</h1>'));
+
+        $container.append($page);
+
+    }
+
+    protected _displayPageWait() {
+        let $container = $('#container');
+
+        $container.empty();
+
+        let $page = $('<div id="page_wait">');
+        $page.append($('<h1>PLEASE WAIT</h1>'));
+
+        $container.append($page);
+    }
+
+    protected _displayPageGame(playersNames: string[], playersScores: number[]) {
+        let $container = $('#container');
+
+        $container.empty();
+
+        let $pageGame = $('<div id="page_game">');
+        for (let i: number = 0; i < playersNames.length; ++i) {
+            let $playerDiv = $('<div class="player_container player_' + i + ' js-player-container hidden">').data('playerId', i);
+            let $playerName = $('<h1 class="js-player-name">').append(playersNames[i]);
+            let $playerScore = $('<span class="player_score js-player-score">10</span>').append(playersNames[i]);
+            $playerDiv.append($playerName);
+            $playerDiv.append($playerScore);
+            $pageGame.append($playerDiv);
+        }
+
+        $container.append($pageGame);
+
+
+        // get all player column
+        let allPlayers = $pageGame.find('.js-player-container');
+        
+        // reset class 
+        allPlayers.addClass('hidden').removeClass('active').removeClass('lock');
+        let $timer = $pageGame.find('.js-timer');
+        $timer.addClass('hidden');
+        // reste timer
+        clearInterval(this._timerInterval);
+
+        // init player name and Score
+        for (let i = 0; i < allPlayers.length; ++i) {
+            let $currentPlayer = $(allPlayers[i]);
+            $currentPlayer.find('.js-player-name').text(playersNames[i]);
+            $currentPlayer.removeClass('hidden');
+
+            if (i < playersScores.length) {
+                $currentPlayer.find('.js-player-score').text(playersScores[i]);
+            } else {
+                $currentPlayer.find('.js-player-score').text(0);
+            }
+        }
+
+    }
+
+    protected _displayPageScore(playersScores: Array<{name: string, score: number}>) {
+        let $container = $('#container');
+
+        $container.empty();
+
+        let $pageScore = $('<div id="page_score">');
+
+        for (let i: number = 0; i < playersScores.length; ++i) {
+            let $placeDiv = $('<div class="place place_' + i + ' js-place-' + i + '">');
+            let $playerName = $('<h1 class="js-player-name">');
+            let $playerScore = $('<h1 class="js-player-score">');
+            $placeDiv.append($playerName);
+            $placeDiv.append($playerScore);
+            $pageScore.append($placeDiv);
+        }
+
+        $container.append($pageScore);
+
+
+        for (let i = 0; i < playersScores.length; ++i) {
+            let $place = $pageScore.find('.js-place-' + (i + 1));
+            $place.find('.js-player-name').text(playersScores[i].name);
+            $place.find('.js-player-score').text(playersScores[i].score);
+            $place.css('background-color',  $container.find('#page_game [data-player-id="' + playersScores[i].playerId + '"]').css('background-color'));
+        }
+    }
 }
